4921ad35b50e774ffa7e35a7528c4d919fa6fa68
niket_panjwani@mckinsey.com <niket_panjwani@mckinsey.com>
2022-12-16 16:56:17 -0600

    Creating migrations

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,8 @@ const config: PostgresConnectionOptions = {
   password: 'Admin123',
   database: 'api',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
 };
 
 export default config;
