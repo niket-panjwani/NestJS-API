d7c968cadd7b9e98042c2dfd2f8d3d1a70824ed3
niket_panjwani@mckinsey.com <niket_panjwani@mckinsey.com>
2022-12-19 00:24:55 -0600

    Creating migrations for user entity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserEntity } from './user.entity';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserEntity> {
     console.log('create user', createUserDto);
     return this.userService.createUser(createUserDto);
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/createUser.dto';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserService {
-  async createUser(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const newUser = new UserEntity();
+    Object.assign(newUser, createUserDto);
+    return await this.userRepository.save(newUser);
   }
 }
